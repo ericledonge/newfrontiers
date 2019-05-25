0b1ab4c4b3b70ca3bf397902d32ecc9b5aa09bbc
[Eric Le Donge] <eric.ledonge@gmail.com>
2019-05-24 21:41:07 -0400

    2nd commit - 3 first tiles random

--- newfrontiers.py
+++ newfrontiers.py
@@ -1,1 +1,9 @@
-print('Hello world')
+import random
+
+tuileA = ['Expeditionary Force', 'Survey Team'] 
+tuileB = ['Export Duties', 'Mining Robots']
+tuileC = ['Alien Monument', 'Alien Researchers']
+
+print('Tuile A: ' + random.choice(tuileA))
+print('Tuile B: ' + random.choice(tuileB))
+print('Tuile C: ' + random.choice(tuileC))
