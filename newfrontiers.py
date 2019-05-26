132a10be58881b847537c84e24f8740df25caae4
[Eric Le Donge] <eric.ledonge@gmail.com>
2019-05-26 15:56:01 -0400

    Add players boards management and first player management too

--- newfrontiers.py
+++ newfrontiers.py
@@ -21,7 +21,7 @@ tuileN = ['Galactic Imperium', 'Imperium Lords']
 tuileO = ['Federation Survey', 'Frontier Settlers']
 tuileP = ['Galactic Federation', 'New Economy']
 
-console.clear()
+playerBoards = [1, 2, 3, 4, 5, 6, 7, 8]
 
 print('Tuile A: ' + random.choice(tuileA))
 print('Tuile B: ' + random.choice(tuileB))
@@ -39,3 +39,14 @@ print('Tuile M: ' + random.choice(tuileM))
 print('Tuile N: ' + random.choice(tuileN))
 print('Tuile O: ' + random.choice(tuileO))
 print('Tuile P: ' + random.choice(tuileP))
+
+player1Board = random.choice(playerBoards)
+playerBoards.remove(player1Board)
+player2Board = random.choice(playerBoards)
+
+print('Player 1: ' + str(player1Board))
+print('Player 2: ' + str(player2Board))
+
+startingPlayer = ['Player 1', 'Player 2']
+
+print('Starting player: ' + str(random.choice(startingPlayer)))
