a479b632dd4a55fafc9a672b9cf12796aac065d1
Eric Le Donge <eric.ledonge@gmail.com>
2019-05-28 20:43:59 -0400

    Version web fonctionnelle

--- new_frontiers.py
+++ new_frontiers.py
@@ -1,52 +1,61 @@
 import random
-import console
+#import console
 
-console.clear()
-console.set_font('Verdana', 16)
+#console.clear()
+#console.set_font('Verdana', 16)
 
-tuileA = ['Expeditionary Force', 'Survey Team'] 
-tuileB = ['Export Duties', 'Mining Robots']
-tuileC = ['Alien Monument', 'Alien Researchers']
-tuileD = ['Terraforming Engineers', 'Terraforming Specialists']
-tuileE = ['Diversified Economy', 'Investment Credits']
-tuileF = ['Improved Logistics', 'Terraforming Robots']
-tuileG = ['Drop Ships', 'Galactic Salon']
-tuileH = ['Technology Investors', 'Trade League']
-tuileI = ['Free Trade Association', 'Pan-Galactic Hologrid']
-tuileJ = ['Imperium War Profiteers', 'Mining League']
-tuileK = ['Military Industry', 'New Galactic Order']
-tuileL = ['Alien Tech Institute', 'Galactic Exchange']
-tuileM = ['Pan-Galactic League', 'Uplift Gene Hunters']
-tuileN = ['Galactic Imperium', 'Imperium Lords']
-tuileO = ['Federation Survey', 'Frontier Settlers']
-tuileP = ['Galactic Federation', 'New Economy']
+def setup():
 
-playerBoards = [1, 2, 3, 4, 5, 6, 7, 8]
+    '''Fonction d'initialisation du jeu'''
 
-print('Tuile A: ' + random.choice(tuileA))
-print('Tuile B: ' + random.choice(tuileB))
-print('Tuile C: ' + random.choice(tuileC))
-print('Tuile D: ' + random.choice(tuileD))
-print('Tuile E: ' + random.choice(tuileE))
-print('Tuile F: ' + random.choice(tuileF))
-print('Tuile G: ' + random.choice(tuileG))
-print('Tuile H: ' + random.choice(tuileH))
-print('Tuile I: ' + random.choice(tuileI))
-print('Tuile J: ' + random.choice(tuileJ))
-print('Tuile K: ' + random.choice(tuileK))
-print('Tuile L: ' + random.choice(tuileL))
-print('Tuile M: ' + random.choice(tuileM))
-print('Tuile N: ' + random.choice(tuileN))
-print('Tuile O: ' + random.choice(tuileO))
-print('Tuile P: ' + random.choice(tuileP))
+    output = ''
 
-player1Board = random.choice(playerBoards)
-playerBoards.remove(player1Board)
-player2Board = random.choice(playerBoards)
+    tuileA = ['Expeditionary Force', 'Survey Team'] 
+    tuileB = ['Export Duties', 'Mining Robots']
+    tuileC = ['Alien Monument', 'Alien Researchers']
+    tuileD = ['Terraforming Engineers', 'Terraforming Specialists']
+    tuileE = ['Diversified Economy', 'Investment Credits']
+    tuileF = ['Improved Logistics', 'Terraforming Robots']
+    tuileG = ['Drop Ships', 'Galactic Salon']
+    tuileH = ['Technology Investors', 'Trade League']
+    tuileI = ['Free Trade Association', 'Pan-Galactic Hologrid']
+    tuileJ = ['Imperium War Profiteers', 'Mining League']
+    tuileK = ['Military Industry', 'New Galactic Order']
+    tuileL = ['Alien Tech Institute', 'Galactic Exchange']
+    tuileM = ['Pan-Galactic League', 'Uplift Gene Hunters']
+    tuileN = ['Galactic Imperium', 'Imperium Lords']
+    tuileO = ['Federation Survey', 'Frontier Settlers']
+    tuileP = ['Galactic Federation', 'New Economy']
 
-print('Player 1: ' + str(player1Board))
-print('Player 2: ' + str(player2Board))
+    playerBoards = [1, 2, 3, 4, 5, 6, 7, 8]
 
-startingPlayer = ['Player 1', 'Player 2']
+    output += 'Tuile A: ' + random.choice(tuileA)
+    output += '<br>Tuile B: ' + random.choice(tuileB)
+    output += '<br>Tuile C: ' + random.choice(tuileC)
+    output += '<br>Tuile D: ' + random.choice(tuileD)
+    output += '<br>Tuile E: ' + random.choice(tuileE)
+    output += '<br>Tuile F: ' + random.choice(tuileF)
+    output += '<br>Tuile G: ' + random.choice(tuileG)
+    output += '<br>Tuile H: ' + random.choice(tuileH)
+    output += '<br>Tuile I: ' + random.choice(tuileI)
+    output += '<br>Tuile J: ' + random.choice(tuileJ)
+    output += '<br>Tuile K: ' + random.choice(tuileK)
+    output += '<br>Tuile L: ' + random.choice(tuileL)
+    output += '<br>Tuile M: ' + random.choice(tuileM)
+    output += '<br>Tuile N: ' + random.choice(tuileN)
+    output += '<br>Tuile O: ' + random.choice(tuileO)
+    output += '<br>Tuile P: ' + random.choice(tuileP)
 
-print('Starting player: ' + str(random.choice(startingPlayer)))
+    player1Board = random.choice(playerBoards)
+    playerBoards.remove(player1Board)
+    player2Board = random.choice(playerBoards)
+
+    output += '<br><br>Eric: ' + str(player1Board)
+    output += '<br>Noémie: ' + str(player2Board)
+
+    startingPlayer = ['Eric', 'Noémie']
+
+    output += '<br><br>Starting player: ' + str(random.choice(startingPlayer))
+    
+    return output
+    
--- new_frontiers_flask.py
+++ new_frontiers_flask.py
@@ -1,4 +1,4 @@
-import newfrontiers
+import new_frontiers
 
 from flask import Flask
 app = Flask(__name__)
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/test")
+def test():
+    return new_frontiers.setup()
